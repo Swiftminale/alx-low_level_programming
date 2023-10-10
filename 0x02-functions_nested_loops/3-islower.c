0f06d72e224b7dcdce9965524ecbd2842260f4a5
Swiftminale <minalefetene@gmail.com>
2023-10-10 11:05:54 -0700

    first commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * @c: the character to check
  * _islower - checks for lowercase character
+ * c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
