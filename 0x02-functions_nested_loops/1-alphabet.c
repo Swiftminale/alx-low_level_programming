daa4603927138fd01e60dfb1b32eb5434a32276f
Swiftminale <minalefetene@gmail.com>
2023-10-10 10:49:09 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
+{
+return write(1, &c, 1);
+}
+void print_alphabet(void) 
+{
+char letter = 'a';
+while (letter <= 'z') 
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+int main(void) {
+print_alphabet();
+return (0);
+}
+
