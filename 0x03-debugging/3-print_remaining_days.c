e25780e88e855c9083e112eda8adaa15b9958c6b
Swiftminale <minalefetene@gmail.com>
2023-10-12 12:32:13 -0700

    first commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 void print_remaining_days(int month, int day, int year)
 {
-int day_of_year = convert_day(month, day);
-int remaining_days;
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 {
-if (month > 2) // Check if it's after February in a leap year
+if (month >= 2)
 {
-day_of_year++; // Increment day for leap years after February
+day++;
 }
-}
-if (day_of_year > 365)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-printf("Day of the year: %d\n", day_of_year);
-remaining_days = 365 - day_of_year;
-printf("Remaining days: %d\n", remaining_days);
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
-
+}
