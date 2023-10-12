0753c409b9965b8f7f38f55715b255c4c91bc9c4
Swiftminale <minalefetene@gmail.com>
2023-10-12 11:18:28 -0700

    first commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,14 @@
 #include "main.h"
 /**
-   * more_numbers - a function that prints 10 times the numbers, from 0 to 14,
-   * followed by a new line.
-   * Return: Always 0
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void more_numbers(void)
+int main(void)
 {
-int i, j = 0;
-for (; j < 10; j++)
-{
-for (i = 0; i <= 14; i++)
-{
-if (i > 9)
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
-_putchar('\n');
-}
+print_line(0);
+print_line(2);
+print_line(10);
+print_line(-4);
+return (0);
 }
