2cfdb914c5693f7225f9a490ab7f183ca59ea6fb
Swiftminale <minalefetene@gmail.com>
2023-10-23 12:02:51 -0700

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to the byte buffer
- * @b: constant byte
- * @n: number of buffer spaces to be filled
- *
- * Return: s
+ * _memset - a function that fills memory with constant bytes
+ * @s: location to fill
+ * @b: char to fill location with
+ * @n: number of bytes to fill
+ * Return: returns pointer to location filled
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
+char *start = s;
+while (n--)
 {
-s[i] = b;
+*s = b;
+s++;
 }
-return (s);
+return (start);
 }
