5efd75658bbbf161e6c0a232b40bb7acde1f28fe
Swiftminale <minalefetene@gmail.com>
2023-10-30 04:01:56 -0700

    first commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,28 +1,34 @@
-#include "alx.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "bootcamp.h"
 /**
- * alloc_grid - A function that returns a pointer to a
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
+ * alloc_grid - creates 2D array of ints
+ * @width: width of 2D array
+ * @height: height of 2D array
+ * Return: pointer to 2D array or NULL if failure
  */
 int **alloc_grid(int width, int height)
 {
-int **array, i = 0, j = 0;
-if (width <= 0 || height <= 0)
+int **arr;
+int i, j;
+if (width < 1 || height < 1)
 return (NULL);
-array = (int **)malloc(sizeof(int *) * height);
-if (array == NULL)
+arr = malloc(sizeof(int *) * height);
+if (arr == NULL)
 return (NULL);
-for (; i < height; i++)
+for (i = 0; i < height; i++)
 {
-array[i] = (int *)malloc(sizeof(int) * width);
-if (array[i] == NULL)
+arr[i] = malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+for (--i; i >= 0; i--)
+free(arr[i]);
+free(arr);
 return (NULL);
 }
+}
 for (i = 0; i < height; i++)
-for (; j < width; j++)
-array[i][j] = 0;
-return (array);
+for (j = 0; j < width; j++)
+arr[i][j] = 0;
+return (arr);
 }
