9d05bb2fb65270a5bb961d3066572550e6021cef
Swiftminale <minalefetene@gmail.com>
2023-11-08 02:36:21 -0800

    first commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
-
+#include "alx.h"
 /**
-* Return: always 0 (success)
-*
-*/
-void print_name(char *name, void (*f)(char *)) {
+ * print_name - prints a name.
+ * @name: string to print.
+ * @f: fuction to use.
+ *
+ * Return: NOTHING
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (!f || !name)
+return;
 f(name);
-}
-void print_function(char *name) {
-printf("Hello, %s!\n", name);
-}
-int main() {
-char name[] = "John";
-print_name(name, print_function);
-return 0;
 }
