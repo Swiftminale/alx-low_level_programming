4082de8dcf496e3da53f7194d8022b0cf59505cf
Swiftminale <minalefetene@gmail.com>
2023-11-16 08:05:48 -0800

    first commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include "lists.h"
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
- **/
+ * print_list - function that prints all the elements of a list_t list
+ * @h: Const pointer of structure list_t
+ * Return: Number of nodes
+ */
+
 size_t print_list(const list_t *h)
 {
-size_t cont = 0;
-while (h)
+unsigned int i = 0;
+while (h != NULL)
 {
-if (h->str)
-rintf("[%u] %s\n", h->len, h->str);
-else
+if (h->str == NULL)
 printf("[0] (nil)\n");
-cont++;
+else
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
+i++;
 }
-return (cont);
+return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,6 @@
-#ifndef LISTS
-#define LISTS
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#ifndef CALC_H
+#define CALC_H
+
 
 /**
  * struct list_s - singly linked list
@@ -13,6 +11,7 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -25,4 +24,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
